f8545fcb3eb86d88bffbd98c37c621dc505fe226
Vincent <Vsuen123@gmail.com>
2022-12-05 10:44:24 -0500

    clone2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Components/Home";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div  className="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"q>
+        Hello world
+    </div>
+  )
+}
+
+export default Home
